00f94d7c4a3c8c1d8b163db80f8c6c07cde77219
Cameron Taggart <cameron.taggart@gmail.com>
2010-08-24 21:38:08 -0700

    iTunesExt initial commit

---
+++ Blog_2010-08-24.fsx
@@ -0,0 +1,49 @@
+﻿
+#r "iTunes.Net.dll"
+#r @"bin\Debug\iTunesExt.dll"
+
+open System
+open System.IO
+open iTunes
+open iTunesExt
+
+// calling this will launch iTunes if not already open
+let app = iTunesAppClass()
+
+// see what verion of iTunes your running
+app.Version
+
+// control the iTunes app
+app.Play()
+app.Stop()
+app.NextTrack()
+app.BackTrack()
+app.Mute <- true
+app.Mute <- false
+app.BrowserWindow.MiniPlayer <- true
+app.BrowserWindow.MiniPlayer <- false
+
+// print all playlists
+app.Playlists
+|> Seq.iter (fun p -> printfn "%s" p.Name)
+
+// print the user playlists that are not smart
+app.UserPlaylists
+|> Seq.filter (fun p -> false = p.Smart)
+|> Seq.iter(fun p -> printfn "%s" p.Name)
+
+// get a list of tracks that are missing files
+// they are marked with an '!' in the iTunes UI
+let tracksMissingFiles =
+  app.LibraryPlaylist.Tracks.Files
+  |> Seq.filter (fun t -> false = File.Exists t.Location)
+  |> List.ofSeq
+
+// see how many were found
+tracksMissingFiles.Length
+
+// print the list
+tracksMissingFiles |> Seq.iter (fun t -> printfn "%s" t.Name)
+
+// remove the tracks from iTunes
+tracksMissingFiles |> Seq.iter (fun t -> t.Delete())
---
+++ iTunesExt.fs
@@ -0,0 +1,57 @@
+﻿
+module iTunesExt
+
+open System
+open System.IO
+open iTunes
+
+type IITArtworkCollection with
+  member this.Seq
+    with get() : (seq<IITArtwork>) = this |> Seq.cast
+
+type IITEncoderCollection with
+  member this.Seq
+    with get() : (seq<IITEncoder>) = this |> Seq.cast
+
+type IITEQPresetCollection with
+  member this.Seq
+    with get() : (seq<IITEQPreset>) = this |> Seq.cast
+
+type IITPlaylistCollection with
+  member this.Seq
+    with get() : (seq<IITPlaylist>) = this |> Seq.cast
+
+type IITSourceCollection with
+  member this.Seq
+    with get() : (seq<IITSource>) = this |> Seq.cast
+
+type IITTrackCollection with
+  member this.Seq
+    with get() : (seq<IITTrack>) = this |> Seq.cast
+  
+  /// only tracks that are files
+  member this.Files
+    with get() =
+      this.Seq
+      |> Seq.filter (fun t ->  t.Kind = ITTrackKind.ITTrackKindFile)
+      |> Seq.map (fun t -> t :?> IITFileOrCDTrack)
+
+type IITVisualCollection with
+  member this.Seq
+    with get() : (seq<IITVisual>) = this |> Seq.cast
+
+type IITWindowCollection with
+  member this.Seq
+    with get() : (seq<IITWindow>) = this |> Seq.cast
+
+type IiTunes with
+  member this.Playlists
+    with get() = this.Sources.Seq |> Seq.map (fun s -> s.Playlists.Seq) |> Seq.concat
+
+  /// only user playlists that are not a special kind
+  member this.UserPlaylists
+    with get() =
+      this.Playlists
+      |> Seq.filter (fun p ->  p.Kind = ITPlaylistKind.ITPlaylistKindUser)
+      |> Seq.map (fun p -> p :?> IITUserPlaylist)
+      |> Seq.filter (fun p -> p.SpecialKind = ITUserPlaylistSpecialKind.ITUserPlaylistSpecialKindNone)
